2d68d5ebd5b1cc1b635d907cebf204ddbb43f7c5
Jose <16-10882@usb.ve>
2023-02-11 12:59:45 -0400

    fix bug

--- main.c
+++ main.c
@@ -7,36 +7,40 @@
 
 #define MAX_PASSWORD_LENGTH 10
 #define MAX_USER_LENGTH 10
-#define MAX_TABLE_LENGTH 10 
+#define MAX_TABLE_LENGTH 10
 
 int main() {
 
-    // creacion de la tabla hash como un array de listas enlazadas
-    LinkedList* hashTable[MAX_TABLE_LENGTH];
+    /* creacion de la tabla hash como un array de listas enlazadas */
+    LinkedList *hashTable[MAX_TABLE_LENGTH];
     int i;
-    for (i = 0; i < 10; i++){
+    for (i = 0; i < 10; i++) {
         hashTable[i] = create_list();
     }
-    
 
     printf("###################### TEST login #####################\n");
 
-    char username[MAX_USER_LENGTH];
-    char password[MAX_PASSWORD_LENGTH];
     int index;
     for (i = 0; i < 3; i++) {
+/*         char *username = malloc(MAX_USER_LENGTH);
+        char *password = malloc(MAX_PASSWORD_LENGTH);
+ */
+        char *username = malloc(MAX_USER_LENGTH);
+        char *password = malloc(MAX_PASSWORD_LENGTH);
         printf("Nombre de usuario: ");
         scanf("%s", username);
+        fflush(stdin);
         printf("Contraseña: ");
         scanf("%s", password);
+        fflush(stdin);
 
-        // obtener indice de la tabla donde estará contenida
+        /* obtener indice de la tabla donde estará contenida */
         index = hash(password);
 
-        //Crear user
+        /* Crear user */
         User *user = create_user(username, index);
 
-        // almacenar en la tabla hash
+        /* almacenar en la tabla hash */
         insert_list(hashTable[0], user);
     }
 
